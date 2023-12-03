83422083fac7c77c484edcdbafce67738792f5ca
CoffeeKiller <18320210361@163.com>
2023-12-03 18:50:37 +0800

    UPDATE 2023_12_03 18:50:32

---
+++ cafe-handwriting/promise.race.js
@@ -0,0 +1,23 @@
+Promise.myRace = (promises) => {
+    return new Promise((rs, rj) => {
+        promises.forEach((p) => {
+            Promise.resolve(p).then(rs).catch(rj)
+        })
+    })
+}
+
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 500, 1)
+})
+
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 2)
+})
+
+Promise.myRace([promise1, promise2]).then((value)=> {
+    console.log(value)
+})
+
+Promise.myRace([promise1, promise2]).then((value) => {
+    console.log(value)
+})
