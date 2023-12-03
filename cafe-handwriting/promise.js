d11ebeb736dbbbb603d6c1b926a694c9fc21e2c1
CoffeeKiller <18320210361@163.com>
2023-12-03 18:41:58 +0800

    UPDATE 2023_12_03 18:41:54

---
+++ cafe-handwriting/promise.js
@@ -0,0 +1,37 @@
+Promise.myAll = (promises) => {
+    return new Promise((rs, rj) => {
+        let count = 0
+        let result = []
+        const len = promises.length
+
+        promises.forEach((p, i) => {
+            Promise.resolve(p).then((res) => {
+                count += 1
+                result[i] = res
+                if (count === len) {
+                    rs(result)
+                }
+            }).catch(rj)
+        })
+
+    })
+}
+
+let p1 = Promise.resolve()
+let p2 = 2
+let p3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 3)
+})
+let p4 = Promise.reject("出错了")
+
+Promise.myAll([p1, p2, p3]).then((res)=>{
+    console.log(res)
+}).catch((err)=>{
+    console.log("error", err)
+})
+
+Promise.myAll([p1, p2, p4]).then((res)=>{
+    console.log(res)
+}).catch((err)=>{
+    console.log("error", err)
+})
