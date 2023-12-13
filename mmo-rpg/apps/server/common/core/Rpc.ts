aa859083a10b87d16c5736b5b2d5658df8495716
CoffeeKiller <18320210361@163.com>
2023-12-13 22:53:07 +0800

    chore: 处理Rpc相关

--- mmo-rpg/apps/server/common/core/Rpc.ts
+++ mmo-rpg/apps/server/common/core/Rpc.ts
@@ -1,6 +1,7 @@
 import {NetConnectOpts, Socket} from "net";
 import {Logger} from "./Logger";
 import {EventEmitter} from "events";
+import {Pipe} from "./Pipe";
 
 
 let callId = 1
@@ -66,5 +67,30 @@ export class RpcServer extends EventEmitter {
 }
 
 export class RpcClient extends EventEmitter {
+    private socket:Socket
+    // @ts-ignore
+    private calling: Map<number, (value:Pick<IRpcRes, "data" | "error">) => void >= new Map()
+    private pipe = new Pipe()
+    private timeout: number = 5000
+    private heartbeatTimer: any
+
+    // get id() {
+    //     return this.options.id
+    // }
+    // set id(id:number) {
+    //     this.options.id = id
+    // }
+
+    static async Create(options: IRpcClientOptions) {
+        const client = new RpcClient(options)
+        await client.init()
+        return client
+    }
+
+    private init() {
+        return new Promise<void>(async (resolve, reject)=> {
+            const options = this.options as any
+        })
+    }
 
 }
